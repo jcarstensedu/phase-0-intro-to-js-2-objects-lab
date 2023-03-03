107674e8a693376cee7e109ec9c28eb564c344e3
johnathan carstens <jcarstensedu@gmail.com>
2023-03-03 15:35:00 -0700

    alldone

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution in this file!
+// THE CAKE IS A LIE
+
+
+const employee = {
+    name: "Chell",
+    streetAddass: "10400 NE 4th St,Bellecue, WA"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newOdj = { ...employee }
+
+    newOdj[key] = value
+
+    return newOdj
+}
+console.log(employee)
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    employee[key] = value
+
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee }
+
+    delete newEmployee[key];
+
+    return newEmployee
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    delete employee[key]
+
+    return employee
+}
